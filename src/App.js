5da2e87853fde6926139e56908ad41f03665657b
정현우 <junghw86@gmail.com>
2022-09-13 18:02:37 +0900

    app.js 에 직접 입력된 것을 component에 만들어서 넣음

--- src/App.js
+++ src/App.js
@@ -1,51 +1,7 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import Users from "./compoenent/User";
 
-function Users() {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false); // 현재 api 요청중인지 확인
-  const [error, setError] = useState(null);
-
-  const fetchUsers = async () => {
-    try {
-      // 요청이 시작 할 때에는 error 와 users 를 초기화하고
-      setError(null);
-      setUsers(null);
-      // loading 상태를 true 로 바꿉니다.
-      setLoading(true);
-      console.log("call data : execute");
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/users"
-      );
-      console.log("call data : complete");
-      setUsers(response.data); // 데이터는 response.data 안에 들어있습니다.
-    } catch (e) {
-      setError(e);
-    }
-    setLoading(false); // loading이 끝났으니, false
-  };
-
-  useEffect(() => {
-    console.log("------------------------------");
-    fetchUsers();
-    console.log("*******************************");
-  }, []);
-
-  if (loading) return <div>로딩중..</div>;
-  if (error) return <div>에러가 발생했습니다</div>;
-  if (!users) return null;
-  return (
-    <>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            {user.username} ({user.name})
-          </li>
-        ))}
-      </ul>
-      <button onClick={fetchUsers}>다시 불러오기</button>
-    </>
-  );
+function App() {
+  return <Users />;
 }
 
-export default Users;
+export default App;
